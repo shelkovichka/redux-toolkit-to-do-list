e8071ce03f29d687d8730124e2a33713e62dfab5
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2025-02-28 20:31:40 +0100

    bug fixed

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -40,5 +41,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new Dotenv(),
   ],
 };
